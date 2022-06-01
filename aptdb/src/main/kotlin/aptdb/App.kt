92cc0294d82099ee5450eb509adc10c6e275db0e
Matthew Cory <SkittishSloth@users.noreply.github.com>
2022-06-01 14:52:43 -0600

    update App.kt

--- aptdb/src/main/kotlin/aptdb/App.kt
+++ aptdb/src/main/kotlin/aptdb/App.kt
@@ -5,12 +5,6 @@ import aptdb.apt.*
 import arrow.core.*
 import arrow.core.continuations.*
 
-// suspend fun main() = either {
-//   val indexTargets = DefaultAptIndexTargetProvider.indexTargets()
-//   println("Index Targets:")
-//   indexTargets.forEach { println("\t${it.bind().fileName}") }
-// }
-
 suspend fun main() {
   val targets = indexTargets()
   targets.forEach { debug(it) }
