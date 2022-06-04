50bf7f045b496ecbc99e52e43f4b649bc138c547
Matthew Cory <matthewcory1@gmail.com>
2022-06-03 20:41:58 -0600

    Refactored to allow switching test cases more easily.

--- aptdb/src/main/kotlin/aptdb/App.kt
+++ aptdb/src/main/kotlin/aptdb/App.kt
@@ -7,25 +7,33 @@ import arrow.core.*
 import arrow.core.continuations.*
 
 suspend fun main() {
-  indexTargets().fold(
-    { error(it) },
-    { targets -> targets.forEach { debug(it) } }
-  )
+  Test.IndexTargets.go()
 }
 
-suspend fun error(error: ShellError<*>) {
-  println("An error occurred.")
-  println(error)
-}
-
-suspend fun debug(target: ValidatedNel<FieldError, AptIndexTarget>) {
-  target.tap {
-    println("${it.fileName}")
-  }.tapInvalid { l ->
-    println("Invalid")
-    l.forEach { println("\t$it") }
+object Test {
+  object IndexTargets {
+    suspend fun go() {
+      indexTargets().fold(
+        { error(it) },
+        { targets -> targets.forEach { debug(it) } }
+      )
+    }
+    
+    suspend fun error(error: ShellError<*>) {
+      println("An error occurred.")
+      println(error)
+    }
+    
+    suspend fun debug(target: ValidatedNel<FieldError, AptIndexTarget>) {
+      target.tap {
+        println("${it.fileName}")
+      }.tapInvalid { l ->
+        println("Invalid")
+        l.forEach { println("\t$it") }
+      }
+    }
+    
+    suspend fun indexTargets(): Effect<ShellError<*>, IndexTargetResults> =
+      DefaultAptIndexTargetProvider.indexTargets()
   }
 }
-
-suspend fun indexTargets(): Effect<ShellError<*>, IndexTargetResults> =
-  DefaultAptIndexTargetProvider.indexTargets()
